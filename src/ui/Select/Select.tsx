83a8b2f17bd1653f833458a72763bdd9042f6cad
rldnrl <devpark1002@gmail.com>
2022-04-23 15:44:41 +0900

    feat: select 컴포넌트 분리

---
+++ src/ui/Select/Select.tsx
@@ -0,0 +1,5 @@
+import { Select as ChakraSelect, SelectProps } from '@chakra-ui/react'
+
+export default function Select(props: SelectProps) {
+  return <ChakraSelect {...props} width="23.75rem" />
+}
