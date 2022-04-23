dabaa67a42d3d533329a1879e1e7f95f32c6e9c8
rldnrl <devpark1002@gmail.com>
2022-04-23 15:30:03 +0900

    refactor: overload function type

--- src/home/hooks/useInput.ts
+++ src/home/hooks/useInput.ts
@@ -1,8 +1,11 @@
 import { ChangeEvent, useState } from 'react'
 
-export default function useInput<
-  T extends HTMLInputElement | HTMLTextAreaElement
->(): [string, (e: ChangeEvent<T>) => void] {
+export function useInput<T extends HTMLInputElement | HTMLTextAreaElement>(): [
+  string,
+  (e: ChangeEvent<T>) => void
+]
+
+export function useInput<T extends HTMLInputElement | HTMLTextAreaElement>() {
   const [value, setValue] = useState('')
 
   const onChange = (e: ChangeEvent<T>) => {
