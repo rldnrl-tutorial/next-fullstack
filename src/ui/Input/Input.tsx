dc2b0c3d3ad1ea4e34996569aa4228f6b2775047
rldnrl <devpark1002@gmail.com>
2022-04-24 15:58:32 +0900

    fix: placeholder 제거

--- src/ui/Input/Input.tsx
+++ src/ui/Input/Input.tsx
@@ -1,12 +1,5 @@
 import { Input as ChakraInput, InputProps } from '@chakra-ui/react'
 
 export default function Input(props: InputProps) {
-  return (
-    <ChakraInput
-      {...props}
-      placeholder="Full name"
-      padding="1.5rem 1rem"
-      width="23.75rem"
-    />
-  )
+  return <ChakraInput {...props} padding="1.5rem 1rem" width="23.75rem" />
 }
