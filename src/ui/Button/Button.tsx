00512d38098a6242b68684c5dd6798fe5b8865b0
rldnrl <devpark1002@gmail.com>
2022-04-24 16:01:23 +0900

    feat: props 위치 수정

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -3,11 +3,11 @@ import { Button as ChakraButton, ButtonProps } from '@chakra-ui/react'
 export default function Button(props: ButtonProps) {
   return (
     <ChakraButton
-      {...props}
       width="23.75rem"
       backgroundColor="facebook.500"
       color="white"
       padding="1.5rem 1rem"
+      {...props}
     />
   )
 }
--- src/ui/Textarea/Textarea.tsx
+++ src/ui/Textarea/Textarea.tsx
@@ -1,5 +1,5 @@
 import { Textarea as ChakraTextArea, TextareaProps } from '@chakra-ui/react'
 
 export default function Textarea(props: TextareaProps) {
-  return <ChakraTextArea {...props} width="23.75rem" height="15rem" />
+  return <ChakraTextArea width="23.75rem" height="15rem" {...props} />
 }
