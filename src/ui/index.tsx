be5a729aa109c9242b5e56e3bf15f581f0572e76
rldnrl <devpark1002@gmail.com>
2022-04-24 16:28:29 +0900

    feat: add custom select component

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -2,10 +2,10 @@ export { default as Title } from './Title'
 export { default as Input } from './Input'
 export { default as Textarea } from './Textarea'
 export { default as Button } from './Button'
+export { default as Select } from './Select'
 export {
   VStack,
   Container,
-  Select,
   FormControl,
   TableContainer,
   Table,
