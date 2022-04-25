defc2800bf293d22f4fb3db3ead0a2a66f789d62
rldnrl <devpark1002@gmail.com>
2022-04-25 21:26:09 +0900

    refactor: separate form control

---
+++ src/ui/FormControl/FormControl.tsx
@@ -0,0 +1,8 @@
+import {
+  FormControl as ChakraFormControl,
+  FormControlProps,
+} from '@chakra-ui/react'
+
+export default function FormControl(props: FormControlProps) {
+  return <ChakraFormControl width="23.75rem" {...props} />
+}
