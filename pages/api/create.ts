85f059c0e27639a7ae66068d766e9a97ad5db24b
rldnrl <devpark1002@gmail.com>
2022-04-25 21:10:27 +0900

    feat: implement create api

---
+++ pages/api/create.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@/config/prisma'
+
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { name, email, feedbackType, message } = req.body
+
+  try {
+    await prisma.feedback.create({
+      data: {
+        name,
+        email,
+        message,
+        feedbackType,
+      },
+    })
+
+    res.status(200).json({ message: 'submitted successfully' })
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+}
