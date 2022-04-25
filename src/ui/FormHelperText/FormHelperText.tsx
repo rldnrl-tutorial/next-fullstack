83fd00752990b40129cd740b04d5d965142e4806
rldnrl <devpark1002@gmail.com>
2022-04-25 21:51:32 +0900

    feat: implement form helper text

---
+++ src/ui/FormHelperText/FormHelperText.tsx
@@ -0,0 +1,8 @@
+import {
+  FormHelperText as ChakraFormHelperText,
+  HelpTextProps,
+} from '@chakra-ui/react'
+
+export default function FormHelperText(props: HelpTextProps) {
+  return <ChakraFormHelperText {...props} />
+}
