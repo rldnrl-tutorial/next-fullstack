194342fbd36a790b5e85c23e82e403571f48725b
rldnrl <devpark1002@gmail.com>
2022-04-25 10:22:51 +0900

    feat: implement home page

---
+++ src/home/HomePage.tsx
@@ -0,0 +1,71 @@
+import { Title, Container, VStack, Input, Select, Textarea, Button } from '@/ui'
+import { FeedbackClient } from './apis/feedback.service'
+import useInput from './hooks/useInput'
+import useSelect from './hooks/useSelect'
+import { Feedback } from './types/feedback'
+
+export default function HomePage() {
+  const {
+    value: fullName,
+    onChange: onFullNameChange,
+    reset: resetFullName,
+  } = useInput()
+  const {
+    value: email,
+    onChange: onEmailChange,
+    reset: resetEmail,
+  } = useInput()
+  const {
+    value: message,
+    onChange: onMessageChange,
+    reset: resetMessage,
+  } = useInput<HTMLTextAreaElement>()
+  const [feedbackType, onFeedbackTypeChange] = useSelect('feedback')
+
+  const onSubmit = async (data: Feedback) => {
+    await FeedbackClient.create(data)
+    resetFullName()
+    resetEmail()
+    resetMessage()
+  }
+
+  return (
+    <Container padding="10">
+      <VStack spacing="5">
+        <Title>Submit Your Feedback</Title>
+        <Input
+          placeholder="Full name"
+          value={fullName}
+          onChange={onFullNameChange}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={onEmailChange}
+        />
+        <Select value={feedbackType} onChange={onFeedbackTypeChange}>
+          <option value="feedback">Feedback</option>
+          <option value="issue">Issue</option>
+          <option value="idea">Idea</option>
+        </Select>
+        <Textarea
+          placeholder="Message"
+          value={message}
+          onChange={onMessageChange}
+        />
+        <Button
+          onClick={() =>
+            onSubmit({
+              name: fullName,
+              email,
+              message,
+            })
+          }
+        >
+          Submit
+        </Button>
+      </VStack>
+    </Container>
+  )
+}
