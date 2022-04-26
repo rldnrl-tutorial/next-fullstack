6bf869b0e09553125eee28b3353b8ce852245c92
rldnrl <devpark1002@gmail.com>
2022-04-26 19:57:36 +0900

    feat: apply generic

--- src/home/hooks/useSelect/useSelect.ts
+++ src/home/hooks/useSelect/useSelect.ts
@@ -1,8 +1,8 @@
 import { ChangeEvent, useState } from 'react'
 
-export function useSelect(
-  defaultValue?: string
-): [string, (e: ChangeEvent<HTMLSelectElement>) => void]
+export function useSelect<T extends string>(
+  defaultValue?: T
+): [T, (e: ChangeEvent<HTMLSelectElement>) => void]
 
 export function useSelect(defaultValue = '') {
   const [value, setValue] = useState(defaultValue)
