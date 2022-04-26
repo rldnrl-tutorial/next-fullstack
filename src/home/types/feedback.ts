2ead7566ae8af2676b38f27ffe00f3118d8960b1
rldnrl <devpark1002@gmail.com>
2022-04-26 19:59:11 +0900

    feat: add feedback type

--- src/home/types/feedback.ts
+++ src/home/types/feedback.ts
@@ -1,5 +1,10 @@
+import type { FeedbackType } from '@prisma/client'
+
 export interface Feedback {
   email: string
   message: string
+  feedbackType: FeedbackType
   name?: string
 }
+
+export type { FeedbackType }
