79b0949453f70b18359ca27c03c8cac861a0d466
rldnrl <devpark1002@gmail.com>
2022-04-27 21:59:14 +0900

    feat: add id

--- pages/api/feedbacks.ts
+++ pages/api/feedbacks.ts
@@ -7,6 +7,7 @@ export default async function handle(
 ) {
   const feedbacks = await prisma.feedback.findMany({
     select: {
+      id: true,
       email: true,
       message: true,
       name: true,
