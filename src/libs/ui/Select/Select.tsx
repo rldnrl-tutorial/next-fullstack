6e67dc8956a9875e726f25b40cd77eba4799b449
rldnrl <devpark1002@gmail.com>
2022-07-15 13:56:20 +0900

    feat: create select component

--- src/libs/ui/Select/Select.tsx
+++ src/libs/ui/Select/Select.tsx
@@ -1,11 +1,21 @@
-import {
-  forwardRef,
-  Select as ChakraSelect,
-  SelectProps,
-} from '@chakra-ui/react'
+import React, { forwardRef, SelectHTMLAttributes } from 'react'
+import clsx from 'clsx'
 
-const Select = forwardRef<SelectProps, 'select'>((props, ref) => {
-  return <ChakraSelect maxWidth="23.75rem" {...props} ref={ref} />
-})
+type SelectProps = SelectHTMLAttributes<HTMLSelectElement>
+
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  function ForwardRefSelect(props, ref) {
+    return (
+      <select
+        {...props}
+        ref={ref}
+        className={clsx(
+          'block w-full border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 rounded-lg p-2.5 text-sm',
+          props.className
+        )}
+      />
+    )
+  }
+)
 
 export default Select
