c11d5fab1e8de8c4d2c1ec2fb49315a51d8079f7
rldnrl <devpark1002@gmail.com>
2022-07-15 13:55:59 +0900

    feat: create input component

--- src/libs/ui/Input/Input.tsx
+++ src/libs/ui/Input/Input.tsx
@@ -1,15 +1,18 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { forwardRef, InputHTMLAttributes } from 'react'
 
-const Input = forwardRef<HTMLInputElement, ComponentPropsWithoutRef<'input'>>(
-  (props, ref) => {
-    return (
-      <input
-        {...props}
-        className="px-6 py-4 max-w-sm w-full border rounded"
-        ref={ref}
-      />
-    )
-  }
-)
+type InputProps = InputHTMLAttributes<HTMLInputElement>
+
+const Input = forwardRef<HTMLInputElement, InputProps>(function ForwardRefInput(
+  props,
+  ref
+) {
+  return (
+    <input
+      {...props}
+      ref={ref}
+      className="block w-full border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 rounded-lg p-2.5 text-sm"
+    />
+  )
+})
 
 export default Input
