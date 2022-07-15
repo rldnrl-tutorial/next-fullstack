49346b0bae96cc1794f4bcbac85578951b8d4725
rldnrl <devpark1002@gmail.com>
2022-07-15 13:55:50 +0900

    feat: delete form helper component

--- src/libs/ui/FormHelperText/FormHelperText.tsx
+++
@@ -1,8 +0,0 @@
-import {
-  FormHelperText as ChakraFormHelperText,
-  HelpTextProps,
-} from '@chakra-ui/react'
-
-export default function FormHelperText(props: HelpTextProps) {
-  return <ChakraFormHelperText {...props} />
-}
