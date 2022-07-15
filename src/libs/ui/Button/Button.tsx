20d5d7e9efaf64320da4b47e8cfa1bd304e02f4b
rldnrl <devpark1002@gmail.com>
2022-07-15 13:55:19 +0900

    feat: create button component

--- src/libs/ui/Button/Button.tsx
+++ src/libs/ui/Button/Button.tsx
@@ -1,14 +1,16 @@
-import { Button as ChakraButton, ButtonProps } from '@chakra-ui/react'
+import { ButtonHTMLAttributes } from 'react'
+import clsx from 'clsx'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 export default function Button(props: ButtonProps) {
   return (
-    <ChakraButton
-      maxWidth="23.75rem"
-      width="100%"
-      backgroundColor="facebook.500"
-      color="white"
-      padding="1.5rem 1rem"
+    <button
       {...props}
+      className={clsx(
+        'w-full py-3 text-white bg-emerald-600 border border-transparent hover:bg-emerald-800 focus:ring-4 focus:ring-blue-300 disabled:hover:bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:hover:bg-blue-600 group flex h-min items-center justify-center p-0.5 text-center font-medium focus:z-10 rounded-lg text-base',
+        props.className
+      )}
     />
   )
 }
