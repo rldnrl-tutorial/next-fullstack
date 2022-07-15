b98355680c5da41ebd8f3c0d56234481ad89d7e4
rldnrl <devpark1002@gmail.com>
2022-07-15 13:56:32 +0900

    feat: create textarea component

--- src/libs/ui/Textarea/Textarea.tsx
+++ src/libs/ui/Textarea/Textarea.tsx
@@ -1,13 +1,17 @@
-import {
-  forwardRef,
-  Textarea as ChakraTextArea,
-  TextareaProps,
-} from '@chakra-ui/react'
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
 
-const Textarea = forwardRef<TextareaProps, 'textarea'>((props, ref) => {
-  return (
-    <ChakraTextArea maxWidth="23.75rem" height="15rem" {...props} ref={ref} />
-  )
-})
+type TextareaProps = ComponentPropsWithoutRef<'textarea'>
+
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (props, ref) => {
+    return (
+      <textarea
+        {...props}
+        ref={ref}
+        className="block w-full h-40 rounded-lg border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+      />
+    )
+  }
+)
 
 export default Textarea
